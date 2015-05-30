cbabdb265e18a4b963d9464485b226c306056107
palmtale <m@cheku.co>
2015-05-30 17:13:06 +0800

    Update

---
+++ src/main/java/so/glad/storage/CrudRepositoryAgent.java
@@ -0,0 +1,54 @@
+package so.glad.storage;
+
+import com.google.common.collect.Lists;
+import org.springframework.data.repository.CrudRepository;
+
+import java.io.Serializable;
+
+/**
+ * @author palmtale
+ *         on 15/5/30.
+ */
+public abstract class CrudRepositoryAgent<T, I extends Serializable> implements CrudRepository<T, I>{
+
+    private CrudRepository<? extends T, I> crudRepository;
+
+    protected void setCrudRepository(CrudRepository<? extends T, I> crudRepository){
+        this.crudRepository = crudRepository;
+    }
+
+    @Override
+    public T findOne(I i) {
+        return crudRepository.findOne(i);
+    }
+
+    @Override
+    public boolean exists(I i) {
+        return crudRepository.exists(i);
+    }
+
+    @Override
+    public Iterable<T> findAll() {
+        return Lists.newArrayList(crudRepository.findAll());
+    }
+
+    @Override
+    public Iterable<T> findAll(Iterable<I> is) {
+        return Lists.newArrayList(crudRepository.findAll(is));
+    }
+
+    @Override
+    public long count() {
+        return crudRepository.count();
+    }
+
+    @Override
+    public void delete(I i) {
+        crudRepository.delete(i);
+    }
+
+    @Override
+    public void deleteAll() {
+        crudRepository.deleteAll();
+    }
+}
